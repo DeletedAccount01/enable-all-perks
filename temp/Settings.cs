fc5d5b098fde5463fc53663c6197870de3e298d9
DeletedAccount01 <112545613+DeletedAccount01@users.noreply.github.com>
2022-09-01 16:18:30 +0100

    Add files via upload

---
+++ temp/Settings.cs
@@ -0,0 +1,14 @@
+﻿using ModSettings;
+using System.Reflection;
+
+namespace temp
+{
+    internal class tempoption : JsonModSettings
+    {
+        internal static tempoption Instance;
+        [Name("On/off")]
+        [Description("Enable all perks yes/no")]
+        public bool turnallperks = true;
+        
+    }
+}
